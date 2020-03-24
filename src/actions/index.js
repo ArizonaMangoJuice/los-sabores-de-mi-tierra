23b574e3e59544b21952168116f9a9dd6613bdd0
AMJ <isaelwebdeveloper@gmail.com>
2020-03-23 21:59:16 -0600

    added thunk

--- src/actions/index.js
+++ src/actions/index.js
@@ -16,3 +16,4 @@ export function changeBody(body){
         body
     }
 }
+
--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,11 @@
 import rootReducer from '../reducers'
-import {createStore} from 'redux'
+import {createStore, applyMiddleware} from 'redux'
+import thunk from 'redux-thunk'
 
-const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+const store = createStore(
+    rootReducer, 
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+    applyMiddleware(thunk)
+)
 
 export default store 
