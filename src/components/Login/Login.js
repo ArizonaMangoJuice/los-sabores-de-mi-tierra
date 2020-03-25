ae82f889886e103966ae4d5359073744d4b466e2
isael lizama <isaellizama@isaels-Mac-mini.local>
2020-03-25 16:11:40 -0700

    added dispatch and logic to see if the user logs in

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,21 +1,36 @@
-import React, {useEffect} from 'react'
+import React, {useEffect, useState} from 'react'
 import {Redirect} from 'react-router-dom'
 import './Login.css'
 import {connect} from 'react-redux'
-import login from '../../actions'
+import {login} from '../../actions'
+
+const mapStateToProps = state => {
+    return {
+        authToken: state.loginReducer.authToken
+    }
+}
 
 function Login(props){
+    let [username, setUsername] = useState('')
+    let [password, setPassword] = useState('')
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        props.dispatch(login(username,password))
+    }
+
     return (
         <div className='page-container'>
+            {props.authToken ? <Redirect to='/dashboard' /> : null}
             <div className='login-page-container'>
                 <div className='login-container card-hover relative'>
                     <div className='logo half'>
                       <i class="far fa-user"></i>
                     </div>
                     <div className='login-form-container half'>
-                        <form className='login-form'>
-                            <input className='main-color card-hover' type='text' placeholder='Username'/>
-                            <input className='main-color card-hover' type='password' placeholder='Password'/>
+                        <form onSubmit={(e) => handleSubmit(e)} className='login-form'>
+                            <input onChange={(e) => setUsername(e.target.value)} className='main-color card-hover' type='text' placeholder='Username' value={username}/>
+                            <input onChange={(e) => setPassword(e.target.value)} className='main-color card-hover' type='password' placeholder='Password' value={password}/>
                             <button className='login-button card-hover'>Login</button>
                         </form>
                     </div>
@@ -25,4 +40,4 @@ function Login(props){
     )
 }
 
-export default connect()(Login)
+export default connect(mapStateToProps)(Login)
--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,9 @@
 import rootReducer from '../reducers'
 import {createStore, applyMiddleware, compose} from 'redux'
 import thunk from 'redux-thunk'
+import jwtDecode from 'jwt-decode'
+import {loadToken} from '../localStorage/localStorage'
+import { setAuthToken, authSuccess } from '../actions'
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
@@ -8,5 +11,17 @@ const store = createStore(
     rootReducer, 
     composeEnhancer(applyMiddleware(thunk))
 )
+
+
+// grab the authtoken from localStorage
+const authToken = loadToken()
+
+// if it is in local storage store in the store
+if(authToken){
+    const token = authToken;
+    const decodedToken = jwtDecode(token);
+    store.dispatch(setAuthToken(token));
+    store.dispatch(authSuccess(decodedToken.user));
+}
 
 export default store 
