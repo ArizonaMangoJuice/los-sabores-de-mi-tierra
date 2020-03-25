f14b8411c22575e2cb565d5817d3cdbc46cfdd22
isael lizama <isaellizama@isaels-Mac-mini.local>
2020-03-25 13:41:05 -0700

    created basic layout of login component

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -6,8 +6,20 @@ import login from '../../actions'
 
 function Login(props){
     return (
-        <div>
-            hello
+        <div className='page-container border-dev'>
+            <div className='-login-page-container'>
+                <div className='login-container border-dev'>
+                    <div className='logo half border-dev'>
+                      <i class="far fa-user"></i>
+                    </div>
+                    <div className='login-form half border-dev'>
+                        <form>
+                            <input type='text' />
+                            <input type='password'/>
+                        </form>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
