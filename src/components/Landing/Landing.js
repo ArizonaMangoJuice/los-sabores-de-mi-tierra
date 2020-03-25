2950f1029131b009d18f796441214604fd251a3c
isael lizama <isaellizama@isaels-Mac-mini.local>
2020-03-24 20:19:31 -0700

    added basic styling to most recent posts

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -2,7 +2,7 @@ import React, {useEffect} from 'react'
 import { Link, Switch } from 'react-router-dom';
 import {connect} from 'react-redux'
 import { fetchPages } from '../../actions';
-
+import './Landing.css'
 const mapStateToProps = state => {
     return {
         pages: state.landingPage.pages
@@ -15,20 +15,22 @@ function Landing(props) {
     })
 
     let pages = props.pages.map(e => (
-        <div className='latest'>
+        <div className='card border-dev relative'>
+            <div className='words-container'>
                 <p className='landing-page-title'>{e.title}</p>
                 <p className='body-snippet'>{e.body}</p>
+            </div>
         </div>
     ))
-    console.log('inside the pages',pages)
+    
     return (
     <>
-        <div className='recent-posts'>
-            {pages}
-            
-        </div> 
-    <Link to='/dashboard'>Dashboard</Link>
-
+        <div className='recent-container'>
+            <div className='recent-posts'>
+                {pages}
+            </div> 
+        </div>
+        <Link to='/dashboard'>Dashboard</Link>
     </>);
 }
 
