d9d98ed554555b0c6d7d9d01f18f66b9449035f1
isael lizama <isaellizama@isaels-Mac-mini.local>
2020-03-25 19:53:13 -0700

    made logout button able to log out and clear auth token

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,38 +1,46 @@
 import React from 'react'
 import NavButton from '../NavButton/NavButton'
 import './Nav.css'
+import { isCompositeComponent } from 'react-dom/test-utils'
+import {connect} from 'react-redux'
+import { clearAuth } from '../../actions'
 // this will be moved when i use redux
-let colors = ['#e6463c', '#ee7f1e', '#6450c8', '#6cb4dd', '#2fa53e'];
-let text = ['Dashboard', 'Source', 'Forms', 'Pages', 'Logout'];
-let icons = ['fa-home','fa-laptop-code','fa-wpforms','fa-file','fa-power-off'];
+let colors = ['#e6463c', '#ee7f1e', '#6450c8', '#6cb4dd'];
+let text = ['Dashboard', 'Source', 'Forms', 'Pages'];
+let icons = ['fa-home','fa-laptop-code','fa-wpforms','fa-file'];
 let color = Math.floor(Math.random() * Math.floor(5));
-let to = ['home','source', 'forms', 'pages', '']
+let to = ['home','source', 'forms', 'pages']
 
 const borderStyle = {
     borderLeft: `4px solid ${colors[color]}`
 }
 
 
-export default class Nav extends React.Component{
+class Nav extends React.Component{
     render(){
         let NavButtons = colors.map((e, i) => (
             <NavButton iconName={`fas ${icons[i]}`} 
                 name={text[i]} 
                 color={e}
                 border={{borderLeft: `4px solid ${e}`}} 
-                bottom={text.length-1 === i  ? 'bottom' : ''}
                 to={to[i]} />
         ));
 
         return (
             <nav className='dashboard-nav'>
                 {NavButtons}
-                {/* <NavButton iconName="fas fa-home" name="Dashboard" border={borderStyle} />
-                <NavButton iconName="fas fa-laptop-code" name="Source" border={borderStyle}/>
-                <NavButton iconName="fab fa-wpforms" name="forms" border={borderStyle}/>
-                <NavButton iconName="far fa-file" name="pages" border={borderStyle}/>
-                <NavButton iconName="fas fa-power-off" name="logout" bottom='bottom' border={borderStyle}/> */}
+                {/* <NavButton  iconName="fas fa-power-off" color='#2fa53e' name="logout" bottom='bottom' border={}/> */}
+                <button onClick={() => this.props.dispatch(clearAuth())} className='nav-button bottom' style={{borderLeft: '4px solid seagreen'}}>
+                    <div className='icon'>
+                        <i className='seagreen fas fa-power-off'></i>
+                    </div>
+                    <p className='dashboard-nav-words'>
+                        Logout
+                    </p>
+                </button>
             </nav>
         )
     }
 }
+
+export default connect()(Nav)
