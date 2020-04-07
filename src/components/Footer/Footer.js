588749820bd61293a2acc5bb09dd3e1aa71a6d92
AMJ <isaelwebdeveloper@gmail.com>
2020-04-06 19:44:00 -0600

    created footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,11 +6,11 @@ export default function Footer(props){
             <img src='https://firebasestorage.googleapis.com/v0/b/isael-blogs.appspot.com/o/images%2Flogo.png?alt=media' className='footer-logo' />
             <div className='social-bar'>
                 
-                <a href='#'><i className="fab fa-github-square"></i></a>
+                <a href='https://github.com/ArizonaMangoJuice'><i className="fab fa-github-square"></i></a>
                 
-                <a href='#'><i className="fab fa-linkedin"></i></a>
+                <a href='https://www.linkedin.com/in/isael-lizama-9a128b130/'><i className="fab fa-linkedin"></i></a>
                 
-                <a href='#'><i className="fab fa-twitter-square"></i></a>
+                <a href='https://twitter.com/ArizonaMJuice'><i className="fab fa-twitter-square"></i></a>
             </div>
             <p className='footer-p'>© 2020. Isael Lizama. All rights reserved. Website Design by <a href='#'>AMJ</a></p>
         </footer>
