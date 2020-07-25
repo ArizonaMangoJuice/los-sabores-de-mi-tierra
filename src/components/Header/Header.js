9734feb881430f8e0584a1620f29ccdf99322578
AMJ <isaelwebdeveloper@gmail.com>
2020-07-24 23:31:17 -0600

    added template css to header and header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Header.css'
+
+export default function Header(){
+    return (
+        <header className='header'>
+            <img className='logo' alt='los sabores de mi tierra' src={require('../../images/LSDMTLogo.png')}></img>
+            <nav className='header-nav'>
+                <ul className='nav-list'>
+                    <li className='nav-list-font'><a className='link-style' href=''>Features</a></li>
+                    <li className='nav-list-font'><a className='link-style' href=''>Recipes</a></li>
+                    <li className='nav-list-font'><a className='link-style' href=''>Memberships</a></li>
+                    <li className='sign-in nav-list-user-button sign-in'>Sign In</li>
+                    <li className='sign-up nav-list-user-button sign-up'>Sign Up</li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
