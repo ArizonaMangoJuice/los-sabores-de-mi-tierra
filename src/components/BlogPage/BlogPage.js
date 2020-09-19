2500ed95ea44a8bcd97fd99638bd3a3172b4522d
AMJ <isaelwebdeveloper@gmail.com>
2020-09-18 20:09:42 -0600

    added blog page paragraph static data

--- src/components/BlogPage/BlogPage.js
+++ src/components/BlogPage/BlogPage.js
@@ -1,13 +1,31 @@
 import React from 'react'
 import BlogPageBanner from '../BlogPageImage/BlogPageImage'
+import BlogPageParagraph from '../BlogPageParagraph/BlogPageParagraph'
 import './BlogPage.css'
+
+// mock data 
+let staticParagraph = [
+    "Audio equidem philosophi vocem, Epicure, sed quid tibi dicendum sit oblitus es. Haec et tu ita posuisti, et verba vestra sunt. Contemnit enim disserendi elegantiam, confuse loquitur. Bona autem corporis huic sunt, quod posterius posui, similiora. Quod cum ita sit, perspicuum est omnis rectas res atque laudabilis eo referri, ut cum voluptate vivatur.",
+    "Quasi vero aut concedatur in omnibus stultis aeque magna esse vitia, et eadem inbecillitate et inconstantia L. Levatio igitur vitiorum magna fit in iis, qui habent ad virtutem progressionis aliquantum. Tertium autem omnibus aut maximis rebus iis, quae secundum naturam sint, fruentem vivere. Materiam vero rerum et copiam apud hos exilem, apud illos uberrimam reperiemus. Atque his de rebus et splendida est eorum et illustris oratio.",
+    "Utinam quidem dicerent alium alio beatiorem. Iam ruinas videres. Non autem hoc, igitur.",
+    "Age, inquies, ista parva sunt. Ad eos igitur converte te, quaeso. Haec para/doca illi, nos admirabilia dicamus. Universa enim illorum ratione cum tota vestra confligendum puto.",
+    "Quos nisi redarguimus, omnis virtus, omne decus, omnis vera laus deserenda est. Sed eum qui audiebant, quoad poterant, defendebant sententiam suam. Fatebuntur Stoici haec omnia dicta esse praeclare, neque eam causam Zenoni desciscendi fuisse. Quisted est autem dignus nomine hominis, qui unum diem totum velit esse in genere isto voluptatis. Ad eos igitur convert te, quaeso. Duo Reges: constructio interrete. An est aliquid per se ipsum flagitiosum, etiamsi nulla comitetur infamia. Quid ad utilitatem tantae pecuniae? Duo enim genera quae erant, fecit tria. Et quod est munus, quod opus sapientiae? Sed in rebus apertissimis nimium longi sumus."
+]
+
+let staticParagraphs = staticParagraph.map( e => 
+    <BlogPageParagraph text={e}/> 
+)
 
 export default function BlogPage(props){
     return (
         <>
             <div className='blog-page-container'>
                 <BlogPageBanner />
+                <div className='blog-page-text-container'>
+                    {staticParagraphs}
+                </div>
             </div>
         </>       
     )
 }
+
--- src/components/BlogPageParagraph/BlogPageParagraph.js
+++ src/components/BlogPageParagraph/BlogPageParagraph.js
@@ -1,44 +1,11 @@
 import React from 'react'
 
-function BlogPageParagraph(props){
-    let result = []
-    let final = []
-
-    if(props && props.paragraph){    
-        let {linkStack} = props
-        let paragraphArray = props.paragraph.split(/[[\]]+/)
-
-        for(let i = 0; i < paragraphArray.length; i++){
-            for(let k = 0; k < linkStack.length; k++){
-                let linkStackName = linkStack[k].name + ', ' + linkStack[k].count
-                if(linkStackName ===  paragraphArray[i]) {
-                    final.push(
-                         <a className='paragraph-link' key={linkStack[k].name + ', ' + linkStack[k].count} href={linkStack[k].link}>{linkStack[k].name}</a>
-                    )
-                }
-        } 
-        }
-        
-        for(let i = 0; i < paragraphArray.length; i++){
-
-            for(let k = 0; k < final.length; k++){
-                if(paragraphArray[i] ===  final[k].key){
-                        paragraphArray[i] = final[k]
-                    } 
-            }
-            result.push(paragraphArray[i])
-        }
-    }    
-    
-    
-
-
-    
+export default function BlogPageParagraph({text, styling}){
     return (
-        <div className='blog-paragraph'>
-            <p>{result.length !== 0 ? result : props.paragraph}</p>
+        <div className='text'>
+            <p className={`blog-page-paragraph ${styling}`}>
+                {text}
+            </p>
         </div>
     )
 }
-
-export default BlogPageParagraph
