95970977df91e947a4c0038ea21656b825f407af
ArizonaMangoJuice <isaelwebdeveloper@gmail.com>
2020-12-30 11:49:01 -0700

    added some styiling to login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -26,13 +26,21 @@ function Login(props){
             <div className='login-image'></div>
             <div className='login-login-container'>
                 <h1>Welcome Back</h1>
-                <div className='login-form-container half'>
+                <p>Sign Into Your Account</p>
+                {/* /login-form-container half */}
+                    <div className='back-link'>
+                        <div className='left-arrow'></div>
+                        <h4>Back to Homepage</h4>
+                    </div>
                     <form onSubmit={(e) => handleSubmit(e)} className='login-form'>
-                        <input onChange={(e) => setUsername(e.target.value)} className='main-color card-hover' type='text' placeholder='Username' value={username}/>
-                        <input onChange={(e) => setPassword(e.target.value)} className='main-color card-hover' type='password' placeholder='Password' value={password}/>
+                        <input onChange={(e) => setUsername(e.target.value)} className='login-input-text' type='text' placeholder='Username' value={username}/>
+                        <input onChange={(e) => setPassword(e.target.value)} className='login-input-text' type='password' placeholder='Password' value={password}/>
                         <button className='login-button card-hover'>Login</button>
                     </form>
-                </div>
+                    {/* this will be random quotes soon tm */}
+                    <div className='sign-in-quotes'>
+                        <p><span>LSDMT.</span> Recipes, thoughts, ideas.</p> 
+                    </div>
             </div>
             {/* {props.authToken ? <Redirect to='/dashboard' /> : null}
             <div className='login-page-container'>
